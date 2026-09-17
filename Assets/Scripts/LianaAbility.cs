6d4f3c3f2cbbbcdbb3bdd3aee605bce0769dcef3
HavenMakesGames <havenjoue1809@gmail.com>
2026-09-17 20:08:35 +0200

    Commentaires
    J'ai ajouté des commentaires au script pour que tu les comprennes , si t'as des questions demande

--- Assets/Scripts/LianaAbility.cs
+++ Assets/Scripts/LianaAbility.cs
@@ -13,19 +13,13 @@ using static UnityEngine.UI.Image;
 public class LianaAbility : MonoBehaviour
 {
     /*
-   Grappin : 
-   Créer un Joint => Maintenir un distance entre un objet A et un objet B , ici le joueur et le point d'accroche du grappin
-   Besoin de :
-   La longueur max du grappin 
-   Un layerMask pour pas s'accrocher a tout  
-   un Vector2 pour la position du point d'accroche
+    Grappin : 
+    Tire le joueur dans la direction de la sourie avec une certaine force 
 
-   Organisation du code :
-   Raycast2D pour trouver le point d'accroche 
-   Creer le point d'accroche 
-   Ajouter une force dans la direction 
    */
 
+
+    // Toutes les stats qui regle le grappin
     [Header("Grappling ")]
     InputAction grapplingInput;
     private bool canUseAbility; 
@@ -37,57 +31,70 @@ public class LianaAbility : MonoBehaviour
     public Vector2 raycastOffset ;
     public float grapPower;
 
-    [Header("Force")]
+    // Les components qui vont faire bouger le perso 
+    [Header("Component")]
     private GameObject player;
     private Rigidbody2D rb;
 
     private void Start()
-    {
-        mouse = Mouse.current;
-        player = gameObject;
-        grapplingInput = InputSystem.actions.FindAction("Liane Ability");
+    { 
+        mouse = Mouse.current; // Connecte la variable avec le peripherique 
+
+        // Connecte les components avec les variables 
+        player = gameObject;  
         rb = gameObject.GetComponent<Rigidbody2D>();
-        canUseAbility = true;
+
+        grapplingInput = InputSystem.actions.FindAction("Liane Ability"); // Lie l'input system avec la variable Input
+        canUseAbility = true; //Sert a reguler l'utilisation de la capacité 
     }
 
     private void Update()
     {
+        // Si la touche est appuyé et la régulation l'autorise 
         if (grapplingInput.WasPressedThisFrame() && canUseAbility == true)
         {
             canUseAbility = false;
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(mouse.position.ReadValue());
-            Vector2 playerPos = player.transform.position;
-            Vector2 direction = (mousePos - playerPos).normalized;
 
-            RaycastHit2D hit = Physics2D.Raycast(playerPos, direction, maxLianaLenght, mask);
-            Debug.DrawRay(playerPos + raycastOffset, direction * 10, Color.green , 2f);
+            // Récupere les positions nécessaire à l'utilisation du grappin 
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(mouse.position.ReadValue());// La ou la sourie est 
+            Vector2 playerPos = player.transform.position;// La ou le joueur est 
+            Vector2 direction = (mousePos - playerPos).normalized;// La direction dans laquelle le grappin doit aller , du joueur vers la sourie 
+
+            // Utilise un Raycast ("laser") pour simuler le grappin et detecter les collisions 
+            RaycastHit2D hit = Physics2D.Raycast(playerPos, direction, maxLianaLenght, mask);// Limiter par la longueur maximale du grappin et un filtre pour ne pas s'accrocher à tout 
+            Debug.DrawRay(playerPos + raycastOffset, direction * 10, Color.green, 2f); // Affiche le grappin temporaire 
             Debug.Log(direction);
 
+            // Si le grappin touche bel et bien un object valide 
             if (hit.collider != null)
             {
+                // Effectue l'animation et l'action du grappin 
                 rb.simulated = false;
-                StartCoroutine(ImobilisationAnimation(hit , playerPos , grapPower));
+                StartCoroutine(ImobilisationAnimation(hit, playerPos, grapPower));
             }
-            
-            StartCoroutine(Cooldown());  
+
+            // Cooldown de la capacite pour ne pas spam
+            StartCoroutine(Cooldown());
+
         }
     }
 
     IEnumerator Cooldown()
     {
-        yield return new WaitForSecondsRealtime(cooldownTime);
+        yield return new WaitForSecondsRealtime(cooldownTime); // Attend le temps de la variable 
         canUseAbility = true;
     }
 
     IEnumerator ImobilisationAnimation(RaycastHit2D hit, Vector2 playerPos , float grapPower)
     {
 
-        yield return new WaitForSecondsRealtime(0.25f);
+        yield return new WaitForSecondsRealtime(0.25f); // Imobilise le perso dans les airs 
         rb.simulated = true;
-        Debug.Log("Le raycast a touché" + hit.collider.name);
-        Vector2 impulsion = (hit.point - playerPos).normalized;
-        rb.AddRelativeForce(impulsion * grapPower * 100);
+        Debug.Log("Le raycast a touché" + hit.collider.name); 
+        Vector2 impulsion = (hit.point - playerPos).normalized; // Calcule la puissance du saut 
+        rb.AddRelativeForce(impulsion * grapPower * 100); // Ajuste et effectue le saut selon la variable grapPower
 
     }
+
 }
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,81 +8,76 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Les infos qui regle comment le perso se deplace 
     [Header("Statistiques")]
     public float speed = 5;
     public float jumpForce = 5;
-    public float gravity = 5;
     public bool isGrounded = true;
     private Vector2 direction;
 
-
+    // Les Inputs des Actions 
     [Header("Actions")]
     InputAction move;
     InputAction jump;
 
-
-    
-
+    // Les components qui controle et limite les deplacements du perso
     [Header("Components")]
     public Rigidbody2D rb;
     private Collider2D col;
     private GameObject player;
 
+    // Catéforie pour quand y'aura des Prefabs
     [Header("Prefabs")]
-    private string eviteBug;
+    private string sertARien;
 
 
     private void Start()
     {
+        // Lie l'input system avec les variables du script
         move = InputSystem.actions.FindAction("Move");
         jump = InputSystem.actions.FindAction("Jump");
 
-
-
+        // Lie les components avec les variables du script
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
         player = GetComponent<GameObject>();
-
-
-
     }
 
     private void Update()
     {
-
-
+        // Lie les valeurs de déplacement . Ex: si le joueur appuie sur A ou D ou bien Espace 
         Vector2 moveValue = move.ReadValue<Vector2>();
         bool jumpValue = jump.IsPressed();
 
+        // Appelle les fonctions pour faire bouger le perso 
         MovePlayer(moveValue);
-
         JumpPlayer(jumpValue);
-
-
     }
 
     private void MovePlayer(Vector2 direction)
     {
+        //Ajoute une force dans la direction du deplacement (A ou D) 
+        // La vitesse dépend de la variable publique speed
         rb.linearVelocity = new Vector2(direction.x * speed , rb.linearVelocity.y);
     }
 
     private bool GetIsGrounded()
     {
+        // Verifie que le perso touche le sol pour pas pouvoir sauter a l'infini
         return Physics2D.Raycast(transform.position, Vector2.down, 1.5f, LayerMask.GetMask("Ground"));
-        
     }
-    public void JumpPlayer(bool jumpValue) 
+
+    public void JumpPlayer(bool jumpValue)
     {
-        
-        if ( jumpValue == true && GetIsGrounded())
+        // Si le joueur appuie sur Espace et est au sol 
+        if (jumpValue == true && GetIsGrounded())
         {
             isGrounded = false;
+            // Ajoute une force vers le haut pour faire sauter le perso 
+            // La force du saut / hauteur est fonction de la variable jumpForce
             rb.AddRelativeForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        
+
         }
     }
-
-   
-
 
 }
